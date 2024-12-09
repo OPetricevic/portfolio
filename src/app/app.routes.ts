5634ede0a814714c20ae16737870ea7ba6109a9d
OPetricevic <omar.petricevic@gmail.com>
2024-12-09 15:43:03 +0100

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ToolsComponent } from './tools/tools.component';
 
-export const routes: Routes = [];
+export const routes: Route[] = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'tools', component: ToolsComponent },
+  { path: '**', redirectTo: '' }, // Fallback ruta
+];
